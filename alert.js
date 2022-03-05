32c2de769df4db279d5b352554f2c62d9dcee3e6
istiakabir <istiakabirtrofder@gmail.com>
2022-03-05 11:19:56 +0600

    first commit

---
+++ alert.js
@@ -0,0 +1,13 @@
+const alertMe = () => {
+    alert('Stop Calling Me, Alert!! Alert!!')
+};
+
+const confirmMe = () => {
+    const response = confirm('You Are 18+');
+    console.log(response);
+};
+
+const promptME = () => {
+    const promptResposse = prompt('Give me Your Email Address');
+    console.log(promptResposse);
+}
